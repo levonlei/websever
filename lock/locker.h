a45206f60fd58da568d114cb2eeb8e2ea9c46cef
YA-LB <67017639+YA-LB@users.noreply.github.com>
2020-11-07 14:17:22 +0800

    Update locker.h

--- lock/locker.h
+++ lock/locker.h
@@ -5,11 +5,15 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+/*
+信号量、互斥锁、条件变量定义
+*/
 class sem
 {
 public:
     sem()
     {
+        //初始化
         if (sem_init(&m_sem, 0, 0) != 0)
         {
             throw std::exception();
@@ -23,13 +27,15 @@ public:
         }
     }
     ~sem()
-    {
+    {//销毁
         sem_destroy(&m_sem);
     }
+    //信号量减一，小于0就挂起等待
     bool wait()
     {
         return sem_wait(&m_sem) == 0;
     }
+    //信号量加一，信号量大于O时唤醒调用post函数的线程
     bool post()
     {
         return sem_post(&m_sem) == 0;
@@ -42,26 +48,26 @@ class locker
 {
 public:
     locker()
-    {
+    {   //初始化
         if (pthread_mutex_init(&m_mutex, NULL) != 0)
         {
             throw std::exception();
         }
     }
     ~locker()
-    {
+    {   //销毁
         pthread_mutex_destroy(&m_mutex);
     }
     bool lock()
-    {
+    {  //加锁
         return pthread_mutex_lock(&m_mutex) == 0;
     }
     bool unlock()
-    {
+    {   //解锁
         return pthread_mutex_unlock(&m_mutex) == 0;
     }
     pthread_mutex_t *get()
-    {
+    {   
         return &m_mutex;
     }
 
@@ -91,6 +97,7 @@ public:
         //pthread_mutex_unlock(&m_mutex);
         return ret == 0;
     }
+    //等待目标条件变量
     bool timewait(pthread_mutex_t *m_mutex, struct timespec t)
     {
         int ret = 0;
