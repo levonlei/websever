7166611fd60d2cb0dc515807150d20f323c1e809
YA-LB <67017639+YA-LB@users.noreply.github.com>
2020-11-07 17:27:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    //需要修改的数据库信息,登录名,密码,库名
-    string user = "root";
-    string passwd = "root";
-    string databasename = "qgydb";
+    //数据库信息
+    string user = "leib";
+    string passwd = "LB20200119";
+    string databasename = "mydb";
 
     //命令行解析
     Config config;
@@ -17,25 +17,18 @@ int main(int argc, char *argv[])
     server.init(config.PORT, user, passwd, databasename, config.LOGWrite, 
                 config.OPT_LINGER, config.TRIGMode,  config.sql_num,  config.thread_num, 
                 config.close_log, config.actor_model);
-    
-
+   
     //日志
     server.log_write();
-
     //数据库
     server.sql_pool();
-
     //线程池
     server.thread_pool();
-
     //触发模式
     server.trig_mode();
-
     //监听
     server.eventListen();
-
     //运行
     server.eventLoop();
-
     return 0;
 }
